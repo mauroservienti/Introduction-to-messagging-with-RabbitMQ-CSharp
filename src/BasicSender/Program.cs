e16918559ba65175f0a78e007247b4b697867830
Mauro Servienti <mauro.servienti@gmail.com>
2022-07-04 11:38:00 +0200

    First demo

---
+++ src/BasicSender/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace BasicSender
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ConfirmSelect();
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "basicreceiver",
+                                     basicProperties: null,
+                                     body: body);
+                channel.WaitForConfirmsOrDie(new TimeSpan(0, 0, 5));
+                
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
